647636a690477d5b5b63ca4b0c03fbdbf81dca6c
Phil <philippe.hermans@two-point-o.be>
2022-06-02 12:01:17 +0200

    fixes for contactbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./Footer";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Contact = () => {
 	return (
-		<>
+		<div className="relative">
 			<div
 				name="contact"
 				className="flex items-center justify-center w-full h-screen p-4 snap-start"
@@ -11,7 +13,7 @@ const Contact = () => {
 				<form
 					method="POST"
 					action="https://getform.io/f/018af295-ffae-4290-8d11-bd3f48e8da30"
-					className="flex flex-col max-w-[600px] w-full"
+					className="flex flex-col max-w-[600px] w-[70%]"
 				>
 					<div className="pb-8">
 						<p className="inline text-5xl font-bold text-red-300">Contact</p>
@@ -49,7 +51,44 @@ const Contact = () => {
 					<Footer />
 				</form>
 			</div>
-		</>
+
+			{/* contactbar */}
+			<div className=" absolute lg:flex  flex-col top-[10em] left-0">
+				<ul>
+					<li className="w-[150px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-indigo-400">
+						<a
+							className="flex items-center justify-between w-full text-gray-300"
+							href="https://www.linkedin.com/in/philippe-hermans90/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Linkedin <FaLinkedin size={22} />
+						</a>
+					</li>
+					<li className="w-[150px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-indigo-400">
+						<a
+							className="flex items-center justify-between w-full text-gray-300"
+							href="https://github.com/Phil22290"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Github <FaGithub size={22} />
+						</a>
+					</li>
+
+					<li className="w-[150px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-indigo-400">
+						<a
+							className="flex items-center justify-between w-full text-gray-300"
+							href="https://drive.google.com/file/d/1NL8q7c5TSliS6kMmkPf9Kbq7zbFqIN2D/view?usp=sharing"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Resume <BsFillPersonLinesFill size={22} />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	);
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,41 +87,6 @@ const Navbar = () => {
 			</ul> */}
 
 			{/* Social icons */}
-			<div className="hidden lg:flex fixed flex-col top-[300%] left-0">
-				<ul>
-					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-indigo-400">
-						<a
-							className="flex items-center justify-between w-full text-gray-300"
-							href="https://www.linkedin.com/in/philippe-hermans90/"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Linkedin <FaLinkedin size={30} />
-						</a>
-					</li>
-					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-indigo-400">
-						<a
-							className="flex items-center justify-between w-full text-gray-300"
-							href="https://github.com/Phil22290"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Github <FaGithub size={30} />
-						</a>
-					</li>
-
-					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-indigo-400">
-						<a
-							className="flex items-center justify-between w-full text-gray-300"
-							href="https://drive.google.com/file/d/1NL8q7c5TSliS6kMmkPf9Kbq7zbFqIN2D/view?usp=sharing"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Resume <BsFillPersonLinesFill size={30} />
-						</a>
-					</li>
-				</ul>
-			</div>
 		</div>
 	);
 };
